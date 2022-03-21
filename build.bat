43a04735af181ecf05f84e02800c848f75d7acaf
Baron-von-Riedesel <japhethx@gmail.com>
2022-03-21 12:58:50 +0100

    tool setm43 added

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 
 rem release version
 jwasm -Zm -bin -Fl=build\wde.lst -Fo=build\wde.com src\wde.asm
+jwasm -mz -Fl=build\setm43.lst -Fo=build\setm43.exe src\setm43.asm
 jwasm -coff -I src\INCLUDE -Fo build\wdevdd.obj src\wdevdd.asm
 jwlink format windows pe dll file build\wdevdd.obj name build\wdevdd.dll lib lib\Kernel32.lib, lib\ntvdm.lib export Init='_Init@0', Dispatch='_Dispatch@0'
 

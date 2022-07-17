243b0faa315d3d2f0e2e5f8b549c26d593e3fc53
Baron-von-Riedesel <japhethx@gmail.com>
2022-07-17 12:35:52 +0200

    changed menu string numbers to symbolic names

--- buildd.bat
+++ buildd.bat
@@ -4,6 +4,6 @@ rem This .BAT builds "debug" versions of wde.com and wdex.com;
 rem "debug" means, these versions display a log (may be redirected to a file).
 rem To create binaries that contain debug info, see buildd2.bat.
 
-if not exist "debug\NUL" mkdir debug
-jwasm -mz -nologo -D?DEBUG=1 -Fl=debug\wde.lst -Fo=debug\WDE.COM src\wde.asm
-jwasm -mz -nologo -D?DEBUG=1 -D?PM=1 -Fl=debug\wdex.lst -Fo=debug\WDEX.COM src\wde.asm
+if not exist "Debug\NUL" mkdir debug
+jwasm -mz -nologo -D?DEBUG=1 -Fl=Debug\wde.lst -Fo=Debug\WDE.COM src\wde.asm
+jwasm -mz -nologo -D?DEBUG=1 -D?PM=1 -Fl=Debug\wdex.lst -Fo=Debug\WDEX.COM src\wde.asm
--- buildd2.bat
+++ buildd2.bat
@@ -4,17 +4,17 @@ rem build debug versions of WDe
 rem this version contains "codeview" debug info that is understood by WD.
 rem WD should be launched with the /SWAP option.
 
-if not exist "debug\NUL" mkdir debug
+if not exist "Debug\NUL" mkdir Debug
 
 rem build the standard version
-jwasm -Zi -nologo -Fl=debug\wde.lst -Fo=debug\WDE.obj src\wde.asm
-jwlink debug c format dos f debug\wde.obj n debug\wde.exe op q,m=debug\wde.map,cvp
+jwasm -Zi -nologo -Fl=Debug\wde.lst -Fo=Debug\WDE.obj src\wde.asm
+jwlink debug c format dos f Debug\wde.obj n Debug\wde.exe op q,m=Debug\wde.map,cvp
 
 rem build the DPMI version
-rem jwasm -Zi -nologo -D?PM=1 -Fl=debug\wdex.lst -Fo=debug\WDEx.obj src\wde.asm
-rem jwlink debug c format dos f debug\wdex.obj n debug\wdex.exe op q,m=debug\wdex.map,cvp
+rem jwasm -Zi -nologo -D?PM=1 -Fl=Debug\wdex.lst -Fo=Debug\WDEx.obj src\wde.asm
+rem jwlink debug c format dos f Debug\wdex.obj n Debug\wdex.exe op q,m=Debug\wdex.map,cvp
 
-rem MS tools
+rem MS tools ( might work better with CodeView )
 rem ml -c -Zi -nologo -FlDebug\wde.lst -FoDebug\WDE.obj src\wde.asm
 rem link16 /CO/NOLOGO/MAP Debug\WDe.obj, Debug\WDe.exe, Debug\WDe.map;
 
